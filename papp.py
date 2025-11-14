bce78a35992c1e845d80107fa9323a4a91a87163
Rahul Jadhav <nyrahul@gmail.com>
2025-11-15 01:03:29 +0530

    added SPIRE installation and bootstrapping
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- papp.py
+++ papp.py
@@ -3,7 +3,7 @@
 import httpx
 
 client_cert = ("certs/client/svid.0.pem", "certs/client/svid.0.key")
-ca_cert = "../certs/server/bundle.0.pem"
+ca_cert = "certs/client/bundle.0.pem"
 
 response = httpx.get("https://fortress.org:8800", verify=ca_cert, cert=client_cert)
 print(response.json())
